ebc12b92c47c36d04f5e9c27cffc4493698cfbd9
Josh <jobr3255@colorado.edu>
2019-04-24 19:02:09 -0600

    Added bootstrap nav

--- server.js
+++ server.js
@@ -3,18 +3,29 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+var favicon = require('serve-favicon');
+// Add favicon
+// app.use(favicon(path.join(__dirname, 'src/assets/images', 'favicon.png')));
+app.use(favicon(path.join(__dirname,'src', 'images','favicon.png')));
+
 var livereload = require('livereload');
 var server = livereload.createServer();
 server.watch(__dirname + "/src");
 
 router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/src/index.html'));
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
 //add the router
 app.use('/', router);
-app.use(express.static(path.join(__dirname, 'src/css')));
+
+// Add css path
+app.use(express.static(path.join(__dirname, 'src', 'css')));
+
+// Add images folder path
+app.use(express.static(path.join(__dirname, 'src', 'images')));
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
